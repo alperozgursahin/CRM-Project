cbb71f991419a6b276136d92e6a8766058465657
Alper Ozgur Sahin <alperozgursahin6@gmail.com>
2024-07-09 14:43:34 +0300

    Refactor RequestForm: Optimize UI and async DB operations

--- crm/Admin/RequestForm.cs
+++ crm/Admin/RequestForm.cs
@@ -1,12 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace crm
@@ -19,150 +14,106 @@ namespace crm
         private string status;
         private string dateTime;
         private string adminUsername;
-        private Color greenYellowColor = Color.GreenYellow;
-        private Color redColor = Color.Red;
-        private Color grayColor = Color.Gray;
         private DateTime startTime;
         private System.Windows.Forms.Timer timer;
+        private readonly Color greenYellowColor = Color.GreenYellow;
+        private readonly Color redColor = Color.Red;
+        private readonly Color grayColor = Color.Gray;
 
-
-        public RequestForm(string customerId, string subject, string description, string status, string dateTime,string adminUsername)
+        public RequestForm(string customerId, string subject, string description, string status, string dateTime, string adminUsername)
         {
             InitializeComponent();
-            this.customerId = customerId    ;
+            this.customerId = customerId;
             this.subject = subject;
             this.description = description;
             this.status = status;
             this.dateTime = dateTime;
             this.adminUsername = adminUsername;
-            
+            InitializeTimer();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void InitializeTimer()
         {
-            DateTime time = DateTime.Now;
-            this.startTime = time;
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            this.startTime = currentTime;
 
             if (status.Equals("Open"))
             {
                 this.status = "In Progress";
-                // Log the start time into database
-                LogActivity(adminUsername, customerId,subject, "Started task", time);
+                await LogActivityAsync(adminUsername, customerId, subject, "Started task", currentTime);
+                await UpdateRequestStatusAsync(customerId, subject, this.status);
 
-                // Update status to In Progress in the database
-                UpdateRequestStatus(customerId);
-
-                labelElapsedTimeText.Text = GetElapsedTime(time);
-
-                // Initialize and start the timer to update elapsed time every second
-                timer = new System.Windows.Forms.Timer();
-                timer.Interval = 1000; // 1 second
-                timer.Tick += Timer_Tick;
+                labelElapsedTimeText.Text = GetElapsedTime(currentTime);
                 timer.Start();
-
-                // Update UI
-                button1.BackColor = redColor;
-                button1.Text = this.status;
-                labelStatusText.Text = this.status;
-
-                
+                UpdateUI(redColor, this.status);
             }
-            else if (this.status.Equals("In Progress"))// Finish the progress
+            else if (status.Equals("In Progress"))
             {
-                // Log the end time into database
                 this.status = "Closed";
+                await UpdateEndTimeAsync(customerId, subject, currentTime);
+                await UpdateRequestStatusAsync(customerId, subject, this.status);
+                await UpdateAdminActivityLogActionToClosedAsync(customerId, subject);
 
-                UpdateEndTime(customerId, time);
-                // Update status to Closed in the database
-                UpdateRequestStatus(customerId);
-
-                UpdateAdminActivityLogActionToClosed(customerId);
-
-                // Update UI
-                button1.BackColor = grayColor; // Or any other color indicating closed state
-                button1.Text = this.status;
-                labelStatusText.Text = this.status;
+                timer.Stop();
+                UpdateUI(grayColor, this.status);
             }
-            
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
         }
 
         private void RequestForm_Load(object sender, EventArgs e)
         {
-            // Fetch the start time from the database
             startTime = GetStartTime(customerId, subject);
+            DisplayRequestDetails();
+            ConfigureFormBasedOnStatus();
+        }
 
-            // Display the data passed from MusteriIstekleriForm
+        private void DisplayRequestDetails()
+        {
             labelCustomerIDText.Text = customerId;
             labelSubjectText.Text = subject;
             labelStatusText.Text = status;
             labelDescriptionText.Text = description;
             labelDateText.Text = dateTime;
+        }
 
-            if (status.Equals("Open"))
+        private void ConfigureFormBasedOnStatus()
+        {
+            switch (status)
             {
-                button1.BackColor = greenYellowColor;
-                button1.Text = "Start Time";
-            }
-            else if (status.Equals("In Progress"))
-            {
-                button1.BackColor = redColor;
-                button1.Text = "In Progress";
-                labelElapsedTimeText.Text = GetElapsedTime(startTime);
-
-                // Initialize and start the timer to update elapsed time every second
-                timer = new System.Windows.Forms.Timer();
-                timer.Interval = 1000; // 1 second
-                timer.Tick += Timer_Tick;
-                timer.Start();
-            }
-            else
-            {
-                button1.BackColor = grayColor;
-                button1.Text = "Closed";
-                labelElapsedTimeText.Text = getTotalTime();
+                case "Open":
+                    UpdateUI(greenYellowColor, "Start Time");
+                    break;
+                case "In Progress":
+                    UpdateUI(redColor, "In Progress");
+                    labelElapsedTimeText.Text = GetElapsedTime(startTime);
+                    timer.Start();
+                    break;
+                case "Closed":
+                    UpdateUI(grayColor, "Closed");
+                    labelElapsedTimeText.Text = GetTotalElapsedTime();
+                    break;
             }
         }
 
-
-
-
-        private void UpdateEndTime(string customerId, DateTime endTime)
+        private void UpdateUI(Color buttonColor, string buttonText)
         {
-            string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
-            string query = @"
-                            UPDATE adminactivitylog
-                            SET EndTime = @endTime
-                            WHERE CustomerId = @customerId AND Subject = @subject";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("@endTime", endTime);
-                cmd.Parameters.AddWithValue("@customerId", customerId);
-                cmd.Parameters.AddWithValue("@subject", this.subject);
-
-                try
-                {
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error updating end time: " + ex.Message);
-                }
-            }
+            button1.BackColor = buttonColor;
+            button1.Text = buttonText;
+            labelStatusText.Text = status;
         }
-        private void LogActivity(string adminUsername, string customerId,string subject, string action, DateTime startTime)
+
+        private async Task LogActivityAsync(string adminUsername, string customerId, string subject, string action, DateTime startTime)
         {
             string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
             string query = @"
-            INSERT INTO AdminActivityLog (AdminUsername, CustomerId, Subject, Action, StartTime)
-            VALUES (@adminUsername, @customerId, @subject, @action, @startTime)";
+                INSERT INTO AdminActivityLog (AdminUsername, CustomerId, Subject, Action, StartTime)
+                VALUES (@adminUsername, @customerId, @subject, @action, @startTime)";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
@@ -175,8 +126,8 @@ namespace crm
 
                 try
                 {
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
                 }
                 catch (Exception ex)
                 {
@@ -185,26 +136,25 @@ namespace crm
             }
         }
 
-        private void UpdateRequestStatus(string customerId)
+        private async Task UpdateRequestStatusAsync(string customerId, string subject, string status)
         {
             string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
             string query = @"
-                            UPDATE supportrequest
-                            SET Status = @status
-                            WHERE CustomerId = @customerId
-                            AND Subject = @subject;";
+                UPDATE supportrequest
+                SET Status = @status
+                WHERE CustomerId = @customerId AND Subject = @subject;";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 MySqlCommand cmd = new MySqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("@status", this.status);
+                cmd.Parameters.AddWithValue("@status", status);
                 cmd.Parameters.AddWithValue("@customerId", customerId);
-                cmd.Parameters.AddWithValue("@subject", this.subject);
+                cmd.Parameters.AddWithValue("@subject", subject);
 
                 try
                 {
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
                 }
                 catch (Exception ex)
                 {
@@ -213,25 +163,51 @@ namespace crm
             }
         }
 
-        private void UpdateAdminActivityLogActionToClosed(string customerId)
+        private async Task UpdateEndTimeAsync(string customerId, string subject, DateTime endTime)
         {
             string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
             string query = @"
-                            UPDATE adminActivityLog
-                            SET Action = 'Closed'
-                            WHERE CustomerId = @customerId
-                            AND Subject = @subject;";
+                UPDATE adminactivitylog
+                SET EndTime = @endTime
+                WHERE CustomerId = @customerId AND Subject = @subject";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@endTime", endTime);
+                cmd.Parameters.AddWithValue("@customerId", customerId);
+                cmd.Parameters.AddWithValue("@subject", subject);
+
+                try
+                {
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error updating end time: " + ex.Message);
+                }
+            }
+        }
+
+        private async Task UpdateAdminActivityLogActionToClosedAsync(string customerId, string subject)
+        {
+            string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
+            string query = @"
+                UPDATE adminActivityLog
+                SET Action = 'Closed'
+                WHERE CustomerId = @customerId AND Subject = @subject;";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 MySqlCommand cmd = new MySqlCommand(query, connection);
                 cmd.Parameters.AddWithValue("@customerId", customerId);
-                cmd.Parameters.AddWithValue("@subject", this.subject);
+                cmd.Parameters.AddWithValue("@subject", subject);
 
                 try
                 {
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
                 }
                 catch (Exception ex)
                 {
@@ -242,13 +218,13 @@ namespace crm
 
         private DateTime GetStartTime(string customerId, string subject)
         {
-            DateTime startTime = DateTime.MinValue; // Default value if not found
+            DateTime startTime = DateTime.MinValue;
             string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
             string query = @"
-                    SELECT CreatedDate
-                    FROM supportRequest
-                    WHERE CustomerId = @customerId AND Subject = @subject
-                    LIMIT 1;";
+                SELECT CreatedDate
+                FROM supportRequest
+                WHERE CustomerId = @customerId AND Subject = @subject
+                LIMIT 1;";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
@@ -279,18 +255,16 @@ namespace crm
             return $"{elapsedTime.Hours}h {elapsedTime.Minutes}m {elapsedTime.Seconds}s";
         }
 
-        private string getTotalTime()
+        private string GetTotalElapsedTime()
         {
-            DateTime startTime1 = DateTime.MinValue; // Default value if not found
-            DateTime endTime = DateTime.MinValue; // Default value if not found
-
+            DateTime startTime = DateTime.MinValue;
+            DateTime endTime = DateTime.MinValue;
             string connectionString = "server=localhost;database=crm_database;uid=root;pwd=1234;";
             string query = @"
-                    SELECT StartTime, EndTime
-                    FROM adminactivitylog
-                    WHERE CustomerId = @customerId
-                    AND Subject = @subject
-                    LIMIT 1";
+                SELECT StartTime, EndTime
+                FROM adminactivitylog
+                WHERE CustomerId = @customerId AND Subject = @subject
+                LIMIT 1";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
@@ -306,8 +280,7 @@ namespace crm
                         if (reader.Read())
                         {
                             if (!reader.IsDBNull(0))
-                                startTime1 = reader.GetDateTime(0);
-
+                                startTime = reader.GetDateTime(0);
                             if (!reader.IsDBNull(1))
                                 endTime = reader.GetDateTime(1);
                         }
@@ -326,15 +299,12 @@ namespace crm
         {
             if (status.Equals("Closed"))
             {
-                // Stop the timer
                 timer.Stop();
             }
             else
             {
-                // Update elapsed time label every second
                 labelElapsedTimeText.Text = GetElapsedTime(startTime);
             }
         }
-
     }
 }
